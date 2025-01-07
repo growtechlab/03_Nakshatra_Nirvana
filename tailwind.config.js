414a356b0031aefe99b9495935f15f6b3d6d80ba
rhtm123 <maurya.iitk@gmail.com>
2025-01-07 17:19:14 +0530

    v0.0.1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './*.html', 
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // require('daisyui'),
+  ],
+}
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        sunteck: './sunteck.html',
+        draditya: './draditya.html',
+        realestate: './realestate.html',
+        multiX: './multiX.html',
+        nakshatra: './nakshatra.html'
+        // Add more HTML files as needed
+      }
+    }
+  }
+})

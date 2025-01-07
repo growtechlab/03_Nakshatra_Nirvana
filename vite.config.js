9f0d748bd4bccbf7af8bbd0519795cf09e6e1ea1
rhtm123 <maurya.iitk@gmail.com>
2025-01-07 17:27:45 +0530

    v2

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: './index.html',
-        sunteck: './sunteck.html',
-        draditya: './draditya.html',
-        realestate: './realestate.html',
-        multiX: './multiX.html',
-        nakshatra: './nakshatra.html'
+        main: './index.html'
         // Add more HTML files as needed
       }
     }
